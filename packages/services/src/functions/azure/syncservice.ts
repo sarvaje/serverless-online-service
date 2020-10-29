411c0414579dd1f1ca9f8f306a91ef5d413f0bd3
Jesus David Garcia Gomez <jegarc@microsoft.com>
2020-10-29 16:19:34 -0700

    New: Use azure functions consumption plan for worker

--- packages/services/src/functions/azure/syncservice.ts
+++
@@ -1,9 +0,0 @@
-import { AzureFunction, Context } from '@azure/functions';
-import { IJob } from '@online-service/utils';
-
-import * as syncService from '../../sync/sync-service';
-
-export const run: AzureFunction = async (context: Context, job: IJob): Promise<void> => {
-    await syncService.run(job);
-    context.done();
-};
